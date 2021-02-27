02621edb24d9cb30b6a8f5713135206c0dfde38b
GwadaKing <davidmiolard@laposte.net>
2021-02-26 22:57:33 -0400

    Conversion package Typescript-->JS

---
+++ myMain.js
@@ -0,0 +1,6 @@
+function main() {
+    "use strict";
+    const audioTable = {"001":"lacascadeauxananas.mp3","002":"lagrotteauxdinosaures.mp3",
+                        "003":"lariviereenchantee","004":"lavalleedesmanguiers","005":"lelacdesnenuphars"};
+    }
+}
